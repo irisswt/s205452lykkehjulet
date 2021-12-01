985c7bcadcb1274b1c64408a75ca6aacef7823d3
irisswt <isakrisager@hotmail.com>
2021-12-01 06:52:11 +0100

    fixed wordgenerator to remove empty chars

--- app/src/main/java/com/example/s205452lykkehjulet/Word.kt
+++ app/src/main/java/com/example/s205452lykkehjulet/Word.kt
@@ -1,5 +1,6 @@
 package com.example.s205452lykkehjulet
 
+import android.util.Log
 import kotlin.random.Random
 
 class Word {
@@ -17,6 +18,9 @@ class Word {
             "by" -> splitWord =  by[Random.nextInt(0,by.size)].uppercase().split("")
             "kodesprog" -> splitWord =  kodesprog[Random.nextInt(0,kodesprog.size)].uppercase().split("")
         }
+        // https://www.tutorialkart.com/kotlin/kotlin-filter-characters-of-string/
+        splitWord = splitWord.filter { it -> !it.equals("") } // split results in empty char at beginning and end of string. Therefore filter is used to remove them
+        Log.e("gword", "generateWord: " + splitWord)
         return splitWord
     }
 }
